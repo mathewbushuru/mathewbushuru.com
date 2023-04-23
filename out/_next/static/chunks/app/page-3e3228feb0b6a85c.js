b2e5117ececcb2b06c1ee982dd78cef9fb83dea1
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2023-04-22 23:22:33 -0700

    Delete out directory

--- out/_next/static/chunks/app/page-3e3228feb0b6a85c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5709:function(e,n,_){Promise.resolve().then(_.t.bind(_,5679,23))},5679:function(e){e.exports={style:{fontFamily:"'__Inter_ccafe3', '__Inter_Fallback_ccafe3'",fontStyle:"normal"},className:"__className_ccafe3"}}},function(e){e.O(0,[667,139,744],function(){return e(e.s=5709)}),_N_E=e.O()}]);
