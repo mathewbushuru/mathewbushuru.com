56bcd91652a40c8ed9efa5b6d9f9ee59875ed270
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-25 15:28:32 -0700

    separate data from info page

---
+++ src/app/info/infopageData.ts
@@ -0,0 +1,106 @@
+export const workExperienceItems = [
+  {
+    title: "Software Engineering Intern at Immersio.io",
+    companyUrl: "https://immersio.io/",
+    timePeriod: "05 / 2022 - 07 / 2022",
+    jobLocation: "Vancouver, Canada",
+    jobType: "Internship, Remote",
+    bulletPoints: [
+      "Integrated a web application for instructors that I had developed as part of my degree's capstone project into the company's existing codebase.",
+      "Implemented chat functionality in the app to support real-time  collaboration of multiple instructors working on a single course, and integrated a chatbot into the app powered by an NLP machine learning model.",
+    ],
+  },
+  {
+    title: "Machine Learning Intern at Cellulant",
+    companyUrl: "https://www.cellulant.io/",
+    timePeriod: "07 / 2019 - 09 / 2019",
+    jobLocation: "Nairobi, Kenya",
+    jobType: "Internship, In person",
+    bulletPoints: [
+      "Built a recommender system using the Surprise Python Library to personalize the company's app for each user.",
+      "Utilized  predictive data models and machine learning techniques to increase the customer retention rate from 47% to 64%. Successfully deployed a machine learning model on AWS to 2 million customers, providing  targeted phone airtime bonuses to customers identified as being at risk of churning within the next 30 days.",
+    ],
+  },
+];
+
+export const extraExperienceItems = [
+  {
+    title: "Frontend Software Developer at UBC Launchpad / BC Cancer Agency",
+    companyUrl: "https://ubclaunchpad.com/",
+    timePeriod: "09 / 2022 - 04 / 2023",
+    jobLocation: "Vancouver, Canada",
+    jobType: null,
+    bulletPoints: [
+      "Contributed to the development of a lab management web application. ",
+      "Implemented a Kanban-style ticket management board with drag and drop capabilities, a PDF  invoice generation tool, form builder components among other features.",
+    ],
+  },
+  {
+    title: "Capstone Project (CPEN491) at UBC ECE / Immersio Inc",
+    companyUrl: "https://ece.ubc.ca/undergraduates/capstone/",
+    timePeriod: "09 / 2021 - 04 / 2022",
+    jobLocation: "Vancouver, Canada",
+    jobType: null,
+    bulletPoints: [
+      "Developed a full-stack React web application featuring intuitive course creation tools for language instructors, utilizing NextJS for the frontend, Express for the server-side logic and MongoDB for the database ",
+    ],
+  },
+  {
+    title: "Fullstack Software Developer at UBC Launchpad",
+    companyUrl: "https://ubclaunchpad.com/",
+    timePeriod: "09 / 2021 - 04 / 2022",
+    jobLocation: "Vancouver, Canada",
+    jobType: null,
+    bulletPoints: [
+      "Contributed to the development of a React Native app to help students manage their finances more effectively. ",
+      "Implemented a feature allowing users to automatically scan  receipts and add expenses using their phone's camera and machine learning/computer vision.",
+    ],
+  },
+  {
+    title: "Control Engineer at UBC Sailbot",
+    companyUrl: "https://www.ubcsailbot.org/",
+    timePeriod: "08 / 2018 - 12 / 2019",
+    jobLocation: "Vancouver, Canada",
+    jobType: null,
+    bulletPoints: [
+      "Contributed to the creation and testing of a robust control system to autonomously control a robotic sailboat. ",
+      "Presented research findings at the 2019  Multidisciplinary Undergraduate Research Conference through a poster presentation",
+    ],
+  },
+];
+
+export const awardItems = [
+  {
+    title: "Full MasterCard Foundation Scholarship ($250,000+)",
+    issuer: "University of British Columbia, MasterCard Foundation",
+    awardUrl: "https://mcfscholars.ubc.ca/",
+    desc: "For academic achievement, leadership skills, and involvement in  community service.",
+  },
+  {
+    title: "2021 International World's Challenge Challenge, Finalist",
+    issuer: "Western University",
+    awardUrl: "https://international.uwo.ca/challenge/global_final/",
+    desc: "This competition brings together students from a wide range of institutions, cultures and continents to form a strong international network and create even greater potential solutions to significant global issues.",
+  },
+  {
+    title:
+      "2021 Mastercard Foundation's  Social Entrepreneurship Fund (~$17000)",
+    issuer: "MasterCard Foundation",
+    awardUrl:
+      "https://mastercardfdn.org/mastercard-foundation-announces-new-fund-to-seed-young-african-leaders-social-ventures/",
+    desc: "This fund enables young changemakers to seed and kick-start promising social ventures and community projects, creating economic opportunities for themselves and others.",
+  },
+  {
+    title: "2020 UBC World's Challenge Challenge, 1st place ($6000)",
+    issuer: "University of British Columbia",
+    awardUrl:
+      "https://apsc.ubc.ca/news/2020/ubc-engineering-students-win-local-worlds-challenge-challenge-competition",
+    desc: "This competition seeks to engage UBC students on international issues and how they can play a part in sparking social change.",
+  },
+  {
+    title: "2019 Global Social Venture Challenge, Winner",
+    issuer: "The Resolution Fellowship",
+    awardUrl: "https://resolutionproject.org/fellows/mathew-bushuru/",
+    desc: "This multi-step competition held in Kigali inspired undergraduate students to propose impactful, socially-responsible projects addressing pressing social issues around the world.",
+  },
+];
