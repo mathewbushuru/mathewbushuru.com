19486cf4165df63cdd15db524bace60cb5c69a4a
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-04-24 17:35:51 -0700

    Fix img build error

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // TODO 
-  images: { unoptimized: true },
+  // TODO
+  images: { unoptimized: true, path: "/" },
   output: "export",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,42 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="scrollbar-hide flex h-full items-end overflow-x-auto">
-      <img
+      <Image
         src="/mockups/ipad/soma_org_ipad.png"
         alt="somaoffline.org"
         width={700}
         height={700 / 1.348}
         className=""
       />
-      <img
+      <Image
         src="/mockups/iphone/soma_org_iphone.png"
         alt="somaoffline.org"
         width={200}
         height={200 * 1.868}
         className="sm:mb-3"
       />
-      <img
+      <Image
         src="/mockups/ipad/google_results_ipad.png"
         alt="somaoffline.org"
         width={700}
         height={700 / 1.348}
         className=""
       />
-      <img
+      <Image
         src="/mockups/iphone/google_iphone.png"
         alt="somaoffline.org"
         width={200}
         height={200 * 1.868}
         className="sm:mb-3"
       />
-      <img
+      <Image
         src="/mockups/ipad/google_results_ipad.png"
         alt="somaoffline.org"
         width={700}
         height={700 / 1.348}
         className=""
       />
-      <img
+      <Image
         src="/mockups/iphone/google_iphone.png"
         alt="somaoffline.org"
         width={200}
