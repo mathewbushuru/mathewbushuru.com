d0186e3e444db5ef9f0ede650202484b639ba5cc
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-20 18:51:00 -0800

    Active nav link button styling

--- components/nav-menu-content.tsx
+++ components/nav-menu-content.tsx
@@ -1,94 +1,48 @@
 import Link from "next/link";
 import {
-  SparklesIcon,
-  PencilLineIcon,
-  NavigationIcon,
-  Wand2Icon,
-  HammerIcon,
-  NewspaperIcon,
-  CameraIcon,
   ArrowUpRightIcon,
   GithubIcon,
   LinkedinIcon,
   MailIcon,
 } from "lucide-react";
 
-import { ThemeToggleButton } from "./theme-toggle-button";
+import { ThemeToggleButton } from "@/components/theme-toggle-button";
+import { NavigationButton } from "@/components/navigation-button";
 import { Button } from "@/components/ui/button";
 
 export const NavMenuContent = () => {
   return (
-    <div className="flex flex-col justify-between h-full">
+    <div className="flex h-full flex-col justify-between">
       {/* Top sidebar  */}
       <div className=" flex min-w-60 flex-col gap-3 px-4 pt-6">
-        <div className="mb-2 flex cursor-pointer flex-col gap-1 rounded-md px-4 py-2 text-sm font-medium hover:bg-accent">
-          <span className="">Mathew Bushuru</span>
-          <span className="font-light text-muted-foreground">
-            Software Engineer
-          </span>
-        </div>
         <Link href="/">
-          <Button className="w-full justify-start" size="sm">
-            <SparklesIcon className="mr-2 h-4 w-4" />
-            <span>Home</span>
-            <span className="ml-auto rounded-sm border bg-secondary px-1.5 text-muted-foreground">
-              1
+          <div className="mb-2 flex cursor-pointer flex-col gap-1 rounded-md px-4 py-2 text-sm font-medium hover:bg-accent">
+            <span className="">Mathew Bushuru</span>
+            <span className="font-light text-muted-foreground">
+              Software Engineer
             </span>
-          </Button>
+          </div>
+        </Link>
+        <Link href="/">
+          <NavigationButton label="Home" keyboardShortcut={1} />
         </Link>
         <Link href="/journey">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <NavigationIcon className="mr-2 h-4 w-4" />
-            <span>Journey</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              2
-            </span>
-          </Button>
+          <NavigationButton label="Journey" keyboardShortcut={2} />
         </Link>
         <Link href="/reading">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <NewspaperIcon className="mr-2 h-4 w-4" />
-            <span>Reading</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              3
-            </span>
-          </Button>
+          <NavigationButton label="Reading" keyboardShortcut={3} />
         </Link>
         <Link href="/writing">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <PencilLineIcon className="mr-2 h-4 w-4" />
-            <span>Writing</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              4
-            </span>
-          </Button>
+          <NavigationButton label="Writing" keyboardShortcut={4} />
         </Link>
         <Link href="/shooting">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <CameraIcon className="mr-2 h-4 w-4" />
-            <span>Shooting</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              5
-            </span>
-          </Button>
+          <NavigationButton label="Shooting" keyboardShortcut={5} />
         </Link>
         <Link href="/all-projects">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <HammerIcon className="mr-2 h-4 w-4" />
-            <span>All projects</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              6
-            </span>
-          </Button>
+          <NavigationButton label="All projects" keyboardShortcut={6} />
         </Link>
         <Link href="/more-about-me">
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <Wand2Icon className="mr-2 h-4 w-4" />
-            <span>More about me</span>
-            <span className="ml-auto rounded-sm border bg-accent px-1.5 text-muted-foreground">
-              7
-            </span>
-          </Button>
+          <NavigationButton label="More about me" keyboardShortcut={7} />
         </Link>
       </div>
 
@@ -107,18 +61,6 @@ export const NavMenuContent = () => {
           </Button>
         </a>
         <a
-          href="https://www.linkedin.com/in/mathewbushuru/"
-          target="_blank"
-          rel="noopener noreferrer"
-          tabIndex={-1}
-        >
-          <Button variant="ghost" className="w-full justify-start" size="sm">
-            <LinkedinIcon className="mr-2 h-4 w-4" />
-            <span>LinkedIn</span>
-            <ArrowUpRightIcon className="ml-auto mr-1.5 h-4 w-4" />
-          </Button>
-        </a>
-        <a
           href="mailto:mathewbushuru@proton.me"
           target="_blank"
           rel="noopener noreferrer"
@@ -127,6 +69,18 @@ export const NavMenuContent = () => {
           <Button variant="ghost" className="w-full justify-start" size="sm">
             <MailIcon className="mr-2 h-4 w-4" />
             <span>Email</span>
+            <ArrowUpRightIcon className="ml-auto mr-1.5 h-4 w-4" />
+          </Button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mathewbushuru/"
+          target="_blank"
+          rel="noopener noreferrer"
+          tabIndex={-1}
+        >
+          <Button variant="ghost" className="w-full justify-start" size="sm">
+            <LinkedinIcon className="mr-2 h-4 w-4" />
+            <span>LinkedIn</span>
             <ArrowUpRightIcon className="ml-auto mr-1.5 h-4 w-4" />
           </Button>
         </a>
---
+++ components/navigation-button.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  SparklesIcon,
+  PencilLineIcon,
+  NavigationIcon,
+  Wand2Icon,
+  HammerIcon,
+  NewspaperIcon,
+  CameraIcon,
+  type LucideIcon,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export const NavigationButton = ({
+  label,
+  keyboardShortcut,
+}: {
+  label: string;
+  keyboardShortcut: number;
+}) => {
+  const pathname = usePathname();
+
+  let Icon: LucideIcon;
+  let isActive = false;
+
+  switch (label) {
+    case "Home":
+      Icon = SparklesIcon;
+      isActive = pathname === "/";
+      break;
+    case "Journey":
+      Icon = NavigationIcon;
+      isActive = pathname === "/journey";
+      break;
+    case "Reading":
+      Icon = NewspaperIcon;
+      isActive = pathname === "/reading";
+      break;
+    case "Writing":
+      Icon = PencilLineIcon;
+      isActive = pathname === "/writing";
+      break;
+    case "Shooting":
+      Icon = CameraIcon;
+      isActive = pathname === "/shooting";
+      break;
+    case "All projects":
+      Icon = HammerIcon;
+      isActive = pathname === "/all-projects";
+      break;
+    case "More about me":
+      Icon = Wand2Icon;
+      isActive = pathname === "/more-about-me";
+      break;
+    default:
+      Icon = SparklesIcon;
+  }
+
+  return (
+    <Button
+      className="w-full justify-start"
+      variant={isActive ? "default" : "ghost"}
+      size="sm"
+    >
+      <Icon className="mr-2 h-4 w-4" />
+      <span>{label}</span>
+      <span className="ml-auto rounded-sm border bg-secondary px-1.5 text-muted-foreground">
+        {keyboardShortcut}
+      </span>
+    </Button>
+  );
+};
