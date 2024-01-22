b63a09867c4a4b15161c27a66ec3e5fd0eb7dd52
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-21 19:19:56 -0800

    Masonry layout for shooting page photos

--- app/shooting/page.tsx
+++ app/shooting/page.tsx
@@ -1,3 +1,5 @@
+import { galleryPagePhotos } from "@/lib/constants";
+
 export const metadata = {
   title: "Mathew Bushuru | Gallery",
 };
@@ -22,205 +24,18 @@ export default function ShootingPage() {
         beauty in the ordinary, and freezing moments in time. I hope you enjoy
         browsing through these images as much as I enjoyed taking them.
       </p>
+
+      <div className="masonry pt-4">
+        {galleryPagePhotos.map((photo) => (
+          <div className="masonry-item" key={photo.imageName}>
+            <img
+              src={`/shooting/${photo.imageName}`}
+              alt={photo.imageName}
+              className="rounded-md"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-const galleryPagePhotos = [
-  {
-    imageName: "ikb-night.jpg",
-    size: 16,
-  },
-  {
-    imageName: "moon.jpg",
-    size: 80,
-  },
-  {
-    imageName: "nairobi-national-park.JPG",
-    size: 110,
-  },
-  {
-    imageName: "lion-gate.jpg",
-    size: 120,
-  },
-  {
-    imageName: "flower.jpg",
-    size: 122,
-  },
-  {
-    imageName: "ponderosa.JPG",
-    size: 134,
-  },
-  {
-    imageName: "east-van.jpg",
-    size: 135,
-  },
-  {
-    imageName: "ubc-nest-lights.jpg",
-    size: 140,
-  },
-  {
-    imageName: "winter-lamp.jpg",
-    size: 172,
-  },
-  {
-    imageName: "orchard-stairs.jpg",
-    size: 186,
-  },
-  {
-    imageName: "radisson-kigali.jpg",
-    size: 188,
-  },
-  {
-    imageName: "lion-gate-flower.jpg",
-    size: 199,
-  },
-  {
-    imageName: "jet.jpg",
-    size: 205,
-  },
-  {
-    imageName: "nairobi-westlands.JPG",
-    size: 210,
-  },
-  {
-    imageName: "ubc-alumni.JPG",
-    size: 213,
-  },
-  {
-    imageName: "downtown-van2.jpg",
-    size: 215,
-  },
-  {
-    imageName: "clock-tv-room.JPG",
-    size: 219,
-  },
-  {
-    imageName: "street-lamp.jpg",
-    size: 225,
-  },
-  {
-    imageName: "celebration-of-light.JPG",
-    size: 236,
-  },
-  {
-    imageName: "rehani.jpg",
-    size: 240,
-  },
-  {
-    imageName: "vgh-window.JPG",
-    size: 258,
-  },
-  {
-    imageName: "ubc-beach.JPG",
-    size: 263,
-  },
-  {
-    imageName: "vpl.JPG",
-    size: 271,
-  },
-  {
-    imageName: "gastown.JPG",
-    size: 284,
-  },
-  {
-    imageName: "kigali.JPG",
-    size: 293,
-  },
-  {
-    imageName: "kits-beach.jpg",
-    size: 291,
-  },
-  {
-    imageName: "siwash-rock.JPG",
-    size: 304,
-  },
-  {
-    imageName: "basketball.JPG",
-    size: 324,
-  },
-  {
-    imageName: "ship-steering.JPG",
-    size: 330,
-  },
-  {
-    imageName: "downtown-van.jpg",
-    size: 346,
-  },
-  {
-    imageName: "ubc-chem.JPG",
-    size: 354,
-  },
-  {
-    imageName: "ubc-macmillan.JPG",
-    size: 361,
-  },
-  {
-    imageName: "winter-tree.JPG",
-    size: 387,
-  },
-  {
-    imageName: "vancouver-letteres-art.JPG",
-    size: 388,
-  },
-  {
-    imageName: "ocean-trees.JPG",
-    size: 429,
-  },
-  {
-    imageName: "home-trees.JPG",
-    size: 444,
-  },
-  {
-    imageName: "kicc.JPG",
-    size: 479,
-  },
-  {
-    imageName: "purple-flowers.jpg",
-    size: 532,
-  },
-  {
-    imageName: "africa-painting.JPG",
-    size: 597,
-  },
-  {
-    imageName: "flowers.JPG",
-    size: 629,
-  },
-  {
-    imageName: "ubc-residence.jpg",
-    size: 678,
-  },
-  {
-    imageName: "snowy-trees.JPG",
-    size: 726,
-  },
-  {
-    imageName: "home-farm.JPG",
-    size: 730,
-  },
-  {
-    imageName: "ikb.jpg",
-    size: 794,
-  },
-  {
-    imageName: "ubc-allard.jpg",
-    size: 831,
-  },
-  {
-    imageName: "bluey-ocean.jpg",
-    size: 905,
-  },
-  {
-    imageName: "bird-ocean.jpg",
-    size: 942,
-  },
-  {
-    imageName: "mountains.jpg",
-    size: 943,
-  },
-  {
-    imageName: "snowy-trees.jpg",
-    size: 1100,
-  },
-];
