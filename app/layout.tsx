d3580e1761706f3cf5d534c17f2fda6aaf3734de
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-23 16:11:53 -0800

    Add anonymized analytics - view count, pages visited, etc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 
@@ -44,12 +45,13 @@ export default function RootLayout({
               <NavMenuContent />
             </div>
             {/* Page content  */}
-            <div className="overflow-scroll scrollbar-hide w-full lg:px-8">
+            <div className="scrollbar-hide w-full overflow-scroll lg:px-8">
               <div className="fixed left-0 right-0 top-0 z-50 lg:hidden">
                 <MobileNavbar />
               </div>
               <div className="mb-4 px-5 pt-16 lg:pt-28">
                 {children}
+                <Analytics />
                 <SpeedInsights />
               </div>
             </div>
