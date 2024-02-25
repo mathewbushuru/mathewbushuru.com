b98867a53596255931807a0fb45343ae200a1c4b
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-02-24 18:32:25 -0800

    Linkedin and Github async redirects

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,20 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/github",
+        destination: "https://github.com/mathewbushuru",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/mathewbushuru/",
+        permanent: true,
+      },
+      {
         source: "/undergrad-thesis",
         destination:
           "https://drive.google.com/file/d/1t95E0oJWrLI9KoxyiY9bYehc0wOcvo-3/view",
-        permanent: true
+        permanent: true,
       },
     ];
   },
