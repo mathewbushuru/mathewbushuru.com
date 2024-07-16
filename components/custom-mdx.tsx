0b9e698199119a16883524f3172ebcdefbcc9ddf
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-07-16 01:44:29 -0700

    writing - add code syntax highlighting

--- components/custom-mdx.tsx
+++ components/custom-mdx.tsx
@@ -1,5 +1,20 @@
 import { MDXRemote, type MDXRemoteProps } from "next-mdx-remote/rsc";
+import { highlight } from "sugar-high";
+
+const components = {
+  code: Code,
+};
 
 export default function CustomMDX(props: MDXRemoteProps) {
-  return <MDXRemote {...props} />;
+  return (
+    <MDXRemote
+      components={{ ...components, ...(props.components || {}) }}
+      {...props}
+    />
+  );
+}
+
+function Code({ children, ...props }: React.HTMLAttributes<HTMLElement>) {
+  const codeHTML = highlight(`${children}`);
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
