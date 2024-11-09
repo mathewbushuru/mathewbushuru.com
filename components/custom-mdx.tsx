846863c62042e3dcf2fee0124faecb399605efc7
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-11-09 17:55:37 +0000

    Experimental TOC for articles

--- components/custom-mdx.tsx
+++ components/custom-mdx.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { MDXRemote, type MDXRemoteProps } from "next-mdx-remote/rsc";
 import rehypeHighlight from "rehype-highlight";
+import rehypeToc from "@jsdevtools/rehype-toc";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
@@ -21,10 +22,37 @@ export default function CustomMDX(props: MDXRemoteProps) {
       options={{
         mdxOptions: {
           remarkPlugins: [],
-          rehypePlugins: [rehypeHighlight],
+          rehypePlugins: [rehypeHighlight, [rehypeToc, { customizeTOC }]],
         },
       }}
       {...props}
     />
   );
 }
+
+const customizeTOC = (toc: any): any => {
+  try {
+    const { children } = toc;
+    const childrenOfChildren = children?.[0]?.children;
+    if (!children?.length || !childrenOfChildren?.length) return null;
+  } catch (e) {}
+  return {
+    type: "element",
+    tagName: "div",
+    properties: { className: "toc" },
+    children: [
+      {
+        type: "element",
+        tagName: "p",
+        properties: { className: "title" },
+        children: [
+          {
+            type: "text",
+            value: "Table of Contents",
+          },
+        ],
+      },
+      ...(toc.children || []),
+    ],
+  };
+};
